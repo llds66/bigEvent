d61dc2154a1c5591eb6fcff551e31f72854e97fe
lldscc <858400061@qq.com>
2024-04-17 09:04:04 +0800

    ✨ feat(注册页): 页面结构

--- src/views/register/index.vue
+++ src/views/register/index.vue
@@ -1,16 +1,109 @@
 <template>
-  <div>
-    Reg.vue 注册界面
+  <!-- 注册页面的整体盒子 -->
+  <div class="reg-container">
+    <!-- 注册的盒子 -->
+    <div class="reg-box">
+      <!-- 标题的盒子 -->
+      <div class="title-box"></div>
+      <!-- 注册的表单区域 -->
+      <el-form :model="regForm" :rules="regRules" ref="regRef">
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input v-model="regForm.username" plcaeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input v-model="regForm.password" plcaeholder="请输入密码" type="password"></el-input>
+        </el-form-item>
+        <!-- 确认密码 -->
+        <el-form-item prop="repassword">
+          <el-input v-model="regForm.repassword" plcaeholder="再次输入密码" type="password"></el-input>
+        </el-form-item>
+         <!-- 注册按钮 -->
+         <el-form-item>
+          <el-button type="primary" class="btn-reg">注册</el-button>
+          <el-link type="info" class="btn-reg">去登录</el-link>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'my-registere'
+  name: 'my-register',
+  data () {
+    const samePwd = (rule, value, callback) => {
+      if (value !== this.regForm.password) {
+        // 如果验证失败，则调用 回调函数时，指定一个 Error 对象。
+        callback(new Error('两次输入的密码不一致!'))
+      } else {
+        // 如果验证成功，则直接调用 callback 回调函数即可。
+        callback()
+      }
+    }
+    return {
+      // 注册数据
+      regForm: {
+        username: '',
+        password: '',
+        repassword: ''
+      },
+      // 校验规则
+      regRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          {
+            pattern: /^[a-zA-Z0-9]{1,10}$/,
+            message: '用户名必须是1-10的大小写字母数字',
+            trigger: 'blur'
+          }],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          {
+            pattern: /^\S{6,15}$/,
+            message: '密码必须是6-15的非空字符',
+            trigger: 'blur'
+          }
+        ],
+        repassword: [
+          { required: true, message: '请再次输入密码', trigger: 'blur' },
+          { pattern: /^\S{6,15}$/, message: '密码必须是6-15的非空字符', trigger: 'blur' },
+          { validator: samePwd, trigger: 'blur' }
+        ]
+      }
 
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.reg-container {
+  background: url('../../assets/images/login_bg.jpg') center;
+  background-size: cover;
+  height: 100%;
 
+  .reg-box {
+    width: 400px;
+    height: 335px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    padding: 0 30px;
+    box-sizing: border-box;
+
+    .title-box {
+      height: 60px;
+      background: url('../../assets/images/login_title.png') center no-repeat;
+    }
+
+    .btn-reg {
+      width: 100%;
+    }
+  }
+}
 </style>
