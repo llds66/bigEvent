8f1b76b7349cd11970674c50070e1fe6e152162d
lldscc <858400061@qq.com>
2024-04-17 18:06:11 +0800

    ✨ feat: 退出功能

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,15 +1,36 @@
 <template>
-<div>
+<div class="container">
   <h1>主页</h1>
+  <el-button @click="logoutFn">退出</el-button>
 </div>
 </template>
 
 <script>
 export default {
-  name: 'my-layout'
+  name: 'my-layout',
+  methods: {
+    // 退出按钮清除token,回到登录页
+    logoutFn () {
+      this.$confirm('您确认退出登录吗？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          // 1. 清空 token
+          this.$store.commit('updateToken', '')
+          // 2. 跳转到登录页面
+          this.$router.push('/login')
+        })
+        .catch((err) => err)
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped lang="less">
+.container{
+  margin-top:100px;
+  text-align: center;
+}
 </style>
